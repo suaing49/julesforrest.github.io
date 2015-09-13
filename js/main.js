6c84e6ee579b8dbfee245aeef05338277b6b6ebf
jules <julesforrest@gmail.com>
2015-09-12 21:47:53 -0700

    fixed arrow position

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 $(window).scroll(function() {
-  var headerHeight = $(window).height();
+  var windowHeight = $(window).height();
+  var pageHeight = $(document).outerHeight();
+  var footerHeight = $('.site-footer').outerHeight();
+  var scrollBottom = $(window).scrollTop() + $(window).height();
+  var headerHeight = $('.nav--container').height();
 
-  if ($(this).scrollTop() > headerHeight) {
+  $('.arrow--container').css("padding-top", headerHeight);
+
+  if ($(this).scrollTop() > windowHeight) {
     $('.homepage .nav--container').fadeIn(300);
   } else {
     $('.homepage .nav--container').fadeOut(300);
   }
+
+  if (scrollBottom + footerHeight > pageHeight) {
+    var margin = pageHeight - (footerHeight + scrollBottom);
+    $('.arrow').css({"margin-top": margin});
+  } else {
+    $('.arrow').css({"margin-top": 0});
+  }
+
 });
